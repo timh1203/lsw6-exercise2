dcc49c00c29451566f92e75abb20302acfe51f25
Timothy Hoang <timh1203@gmail.com>
2018-08-27 23:55:44 -0500

    Remove some src, add css reset, render hello

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
