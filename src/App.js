e06925bea237a608682e9463bbeceb30a750d623
Timothy Hoang <timh1203@gmail.com>
2018-08-28 00:05:06 -0500

    Add code for api and life cycle method

--- src/App.js
+++ src/App.js
@@ -37,4 +37,35 @@ class App extends Component {
   }
 }
 
+
+// USING API TO PULL FROM WIKIPEDIA AND USING LIFE CYCLE METHODS
+// class App extends Component {
+//   state = {
+//       data: []
+//   };
+
+//   // Code is invoked after the component is mounted/inserted into the DOM tree.
+//   componentDidMount() {
+//       const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*";
+
+//       fetch(url)
+//           .then(result => result.json())
+//           .then(result => {
+//               this.setState({
+//                   data: result
+//               })
+//           });
+//   }
+
+//   render() {
+//       const { data } = this.state;
+
+//       const result = data.map((entry, index) => {
+//           return <li key={index}>{entry}</li>;
+//       });
+
+//       return <ul>{result}</ul>;
+//   }
+// }
+
 export default App
