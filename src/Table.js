191c82de3de06edbbee5523b0febfc5a111b4111
Timothy Hoang <timh1203@gmail.com>
2018-08-27 23:58:48 -0500

    Add table component separation

--- src/Table.js
+++ src/Table.js
@@ -2,34 +2,46 @@ import React, { Component } from 'react'
 
 class Table extends Component {
   render() {
+    const TableHeader = () => { 
       return (
-          <table>
-              <thead>
-                  <tr>
-                      <th>Name</th>
-                      <th>Job</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>Charlie</td>
-                      <td>Janitor</td>
-                  </tr>
-                  <tr>
-                      <td>Mac</td>
-                      <td>Bouncer</td>
-                  </tr>
-                  <tr>
-                      <td>Dee</td>
-                      <td>Aspiring actress</td>
-                  </tr>
-                  <tr>
-                      <td>Dennis</td>
-                      <td>Bartender</td>
-                  </tr>
-              </tbody>
-          </table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
       );
+    }
+
+    const TableBody = () => { 
+      return (
+        <tbody>
+          <tr>
+            <td>Charlie</td>
+            <td>Janitor</td>
+          </tr>
+          <tr>
+            <td>Mac</td>
+            <td>Bouncer</td>
+          </tr>
+          <tr>
+            <td>Dee</td>
+            <td>Aspiring actress</td>
+          </tr>
+          <tr>
+            <td>Dennis</td>
+            <td>Bartender</td>
+          </tr>
+        </tbody>
+      );
+    }
+
+    return (
+      <table>
+        <TableHeader />
+        <TableBody />  
+      </table>
+    );
   }
 }
 
